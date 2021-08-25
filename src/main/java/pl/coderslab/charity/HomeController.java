13b003fd013ef9d288f9e00aba7f5f450f333006
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-08-25 21:40:15 +0200

    #2 Akcja home - suma wszystkich przekazanych darów

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -14,19 +20,45 @@ import java.util.List;
 public class HomeController {
 
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
+    private final CategoryRepository categoryRepository;
 
     //instead of this way of adding modelattribute, we can
     //add this list in homeAction as
     // model.addAttribute("institutions",institutionRepository.findAll());
     @ModelAttribute("institutions")
-    public List<Institution> institutions(){return institutionRepository.findAll();}
+    public List<Institution> institutions() {
+        return institutionRepository.findAll();
+    }
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    //j.w.
+    @ModelAttribute("donations")
+    public List<Donation> donations() {
+        return donationRepository.findAll();
+    }
+
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository, CategoryRepository categoryRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
-    public String homeAction(Model model){
+    public String homeAction(Model model) {
+//        Donation donation=new Donation();
+//        donation.setQuantity(5);
+//        donation.setInstitution(institutionRepository.findById(1L).orElse(null));
+//        donation.setCategories(Arrays.asList(categoryRepository.findById(1L).orElse(null)));
+//        donation.setStreet("Jakaśtam");
+//        donation.setCity("Warszawa");
+//        donation.setZipCode("02-822");
+//        donation.setPickUpDate(LocalDate.now());
+//        donation.setPickUpTime(LocalTime.now());
+//        donation.setPickUpComment("Bez komentarza");
+//        donationRepository.save(donation);
+
+        Integer allByQuantity = donationRepository.sumOfQuantities();
+        model.addAttribute("quantity", allByQuantity);
         return "index";
     }
 }
