4e0cfe21651eab3d48d0079970f72198d763cdae
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-08-30 21:39:42 +0200

    DonationController

--- src/main/java/pl/coderslab/charity/DonationController.java
+++ src/main/java/pl/coderslab/charity/DonationController.java
@@ -1,10 +1,21 @@
 package pl.coderslab.charity;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.repository.CategoryRepository;
 import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/donation")
@@ -20,5 +31,29 @@ public class DonationController {
         this.donationRepository = donationRepository;
     }
 
+    @ModelAttribute("institutions")
+    List<Institution> institutions() {
+        return institutionRepository.findAll();
+    }
+
+    @ModelAttribute("categories")
+    List<Category> categories() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/form")
+    public String add(Model model){
+        model.addAttribute("donation",new Donation());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String save(@Valid Donation donation, BindingResult result){
+        if (result.hasErrors()){
+            return "form";
+        }
+        donationRepository.save(donation);
+        return "redirect:/";
+    }
 
 }
